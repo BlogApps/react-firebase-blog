88c30417755dfccd00c6dd90e72d79bfa45ba5b7
Kento75 <kento2github@gmail.com>
2019-02-03 18:36:56 +0900

    Fix dir name admin

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
-import AdminHeader from '../components/Admin/AdminHeader/AdminHeader'
+import AdminHeader from '../components/admin/AdminHeader/AdminHeader'
 
 const PrivateRoute = ({
   isAuthenticated,
